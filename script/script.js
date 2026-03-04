e2508b59f012d691df9faaab563f37c465b11c80
ashik081 <freelancerashik81@gmail.com>
2026-03-04 21:32:15 +0600

    api conneted and display show all cards

--- script/script.js
+++ script/script.js
@@ -5,6 +5,40 @@ const loadLesson = () => {
     .then((data) => displayLesson(data.data));
 };
 
+// level
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevel(data.data));
+};
+
+// level word display
+const displayLevel = (words) => {
+  const cardContainer = document.getElementById("cards-container");
+  cardContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <div class="bg-white rounded-lg text-center px-6 py-8 space-y-3">
+        <h3 class="text-xl font-bold">${word.word}</h3>
+        <p class="text-gray-600 text-sm">meaning/pronunciation</p>
+        <p class="text-xl font-medium font-bangla text-gray-500"> ${word.meaning} / ${word.pronunciation}</p>
+        <div class="flex justify-between items-center gap-5 mt-2">
+          <button class="btn"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
+    `;
+
+    cardContainer.appendChild(card);
+  });
+};
+loadLevelWord();
+
 // display show
 const displayLesson = (lessons) => {
   // 1 - get element and empty
@@ -13,12 +47,10 @@ const displayLesson = (lessons) => {
 
   // 2 - loop chalabo sob gula element er upor
   for (let lesson of lessons) {
-    console.log(lesson);
-
     // div create and innerHTML setup
     const div = document.createElement("div");
     div.innerHTML = `
-        <button class="btn btn-outline btn-primary">
+        <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
           <i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}
         </button>
     `;
